6d301c1f248a87f390423c34ebe80e027f91b864
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-06-11 13:02:37 +0200

    feat: create error handlers and postRoute middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,29 @@
 import "dotenv/config"
 import express from "express"
+import postRoute from "./routes/post.js"
 
 const app = express();
+
+app.use(express.json())
+
+app.use('/posts', postRoute);
+
+
+app.use((req, res, next) => {
+    const error = new Error(`Cannot ${req.method} ${req.originalUrl} - Route not found`)
+    error.status = 404
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+
+    const statusCode = err.status || 500;
+    const message = err.message || "Something went wrong on the server";
+
+    res.status(statusCode).json({
+        error: message
+    });
+});
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
