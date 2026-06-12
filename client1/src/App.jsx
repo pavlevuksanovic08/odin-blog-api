f92c87277e0579d65bee3b3b7ca7da28ec1dee5b
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-06-12 11:24:07 +0200

    feat: fetch and display posts

--- client1/src/App.jsx
+++ client1/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
+import Post from "./Post/Post";
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+      .then(data => setPosts(data))
+      .catch(err => console.log("Fetch failed: " + err))
+  }, []);
 
   return (
     <>
-      
+      <div className="posts">
+        {posts.map(post => {
+          <Post data={post}></Post>
+        })}
+      </div>
     </>
   )
 }
