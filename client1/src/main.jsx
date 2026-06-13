ea5949485b4916e1e47360b415605d5259078c36
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-06-13 10:29:11 +0200

    feat: create react router

--- client1/src/main.jsx
+++ client1/src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
+import PostDetails from './PostDetails/PostDetails.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Your main page with all the post cards
+  },
+  {
+    path: "/posts/:id", // The ":" makes "id" a dynamic variable
+    element: <PostDetails />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
