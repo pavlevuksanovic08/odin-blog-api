6ed801b1c8a435be973cab2c9c68580ec563190d
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-06-13 10:30:25 +0200

    feat: add Link to PostCard and fix styling

--- client1/src/Posts/PostCard/PostCard.jsx
+++ client1/src/Posts/PostCard/PostCard.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import styles from "./PostCard.module.css"
+import { Link } from "react-router-dom"
 
 function PostCard({ data }) {
     return (
-        <div className={styles.post}>
-            <p className={styles.title}>{data.title}</p>
-            <p className={styles.text}>{data.content}</p>
-        </div>
+        <Link to={`/posts/${data.id}`} className={styles.backLink}>
+            <div className={styles.post}>
+                <p className={styles.title}>{data.title}</p>
+                <p className={styles.text}>{data.content}</p>
+            </div>
+        </Link>
     )
 }
 
